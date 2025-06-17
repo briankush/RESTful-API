4e8ab9fcd3c3c856a2759a081021e2dc2c3b28a1
briankush <briankush05@gmail.com>
2025-06-17 03:33:30 +0300

    Start on crud operations

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+app.use(express.json)
 
 app.get('/', (req, res) =>{ //route to handle get request from root url
-    res.send('Hello World i am learning react on a monday') //send a response to client 
+    res.send('Hello World i am learning react on a teusday') //send a response to client 
 });
 
-
+// middlewwares
 app.use((req, res, next) =>{
     console.log(`${req.method} ${req.url}`);
     next();
-});
+}); // end
 // routing , about page
 app.get('/about', (req, res) => {
     res.send('About page')
@@ -24,7 +25,27 @@ app.get('/home', (req, res) => {
 //Contact page
 app.get('/contact', (req, res) =>{
     res.send('Contact page')
+}) // end
+
+// route parameters
+app.get('/user/:id', (req, res) =>{
+    res.send(`User ID: ${req.params.id}`)
 })
+
+//Query params
+app.get('/search', (req, res) =>{
+    res.send(`Search Query: ${req.query.q}`)
+})
+
+
+
+
+//REST API
+//CRUD operations
+
+
+
+
 app.listen(PORT, () =>{
     console.log(`Server is running on http://localhost:${PORT}`)
 });
